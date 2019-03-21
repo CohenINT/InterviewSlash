fcb00746e6a10cb6582dce50eb4f91eff661e090
moshe cohen <moshe22cohen@gmail.com>
2019-03-21 20:29:11 +0200

    compelte UI

--- Ticker-feed/js/main.js
+++ Ticker-feed/js/main.js
@@ -1,4 +1,5 @@
-var data ="";
+
+
 
 function loadData() {
     
@@ -22,3 +23,100 @@ function loadData() {
 
 
 
+function send() {
+
+    var user_input = document.getElementsByClassName("user_input")[0].value;
+    var user_pic = document.getElementsByClassName("typing_user_profile")[0].src;
+    var datetime="";
+    var d = new Date();
+    datetime += d.getDate() + "/" + d.getMonth() + "/" + d.getFullYear() + " " + d.getHours() + ":" + d.getMinutes();
+
+
+  
+
+
+
+}
+
+
+function update()
+{
+
+    
+
+    //get item from the inifinte loop
+    for (var i = 0; i < data.length; i++) {
+        //modifying html according to item
+        var item = data[i];
+        debugger;
+        console.log("this is data");
+        console.log(data);
+        usr.picture = item.picture;
+        usr.date = item.datetime;
+        usr.msg = item.text;
+        usr.name = item.fullname;
+        console.log("this is usr");
+        console.log(usr);
+       
+        var template = '<div class="mt-3 box seperator">' +
+            '   <div class="row">' +
+            ' <div class="col-12" style="margin-top: 2%">' +
+            ' <div class="user_profile_pic float-right mt-2">' +
+            ' <img class="h-100 w-100" src=' +
+            usr.picture.toString() +
+            '/>' +
+            '</div>' +
+            '</div>' +
+            ' </div>' +
+            '  <div class="row" style="margin-top: -12%; margin-right: 6%;">' +
+            ' <div class="col-12">' +
+            '  <div class="container">' +
+            '  <div class="row">' +
+            ' <div class="col-12">' +
+            ' <div class="float-right poster_name float-right">' +
+            usr.name.toString() + '</div>' +
+
+
+            ' </div>' +
+            ' <div class="col-12">' +
+            '  <div class=" poster_date float-right mr-2 ">' + usr.date.toString() + '</div>' +
+
+            ' </div>' +
+            ' <div class="col-12">' +
+            ' <div class="float-right poster_text">' + usr.msg.toString() + '</div>' +
+            '</div>' +
+
+            ' </div>' +
+            '  </div>' +
+
+            '  </div>' +
+            ' </div>' +
+            '  </div>';
+        console.log("template now is: ");
+        console.log(template);
+       
+
+        //inserting the code of the new item into HTML 
+        $("#first_box").prepend(template);
+
+
+        //{doing animation}
+
+
+    }
+
+}
+
+
+
+$(document).ready(init);
+function init() {
+
+    $(".send_btn").click(send);
+
+
+
+
+
+
+}
