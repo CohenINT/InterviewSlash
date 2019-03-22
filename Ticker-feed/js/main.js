db6c008dda30e008f72db9ffa8b60d06cc6e62fe
moshe cohen <moshe22cohen@gmail.com>
2019-03-22 14:56:51 +0200

    fixed update(), interval time works good, inserting html code works good, effect works good

--- Ticker-feed/js/main.js
+++ Ticker-feed/js/main.js
@@ -12,7 +12,7 @@ function loadData() {
         })
         .then(data => {
             // Work with JSON data here
-            console.log(data);
+            stop = data.length;
             this.data = data;
         })
         .catch(err => {
@@ -39,26 +39,52 @@ function send() {
 }
 
 
-function update()
-{
+
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+
+
+
+
+var i = 0;//index for loop
+var stop ;
+
+function update() {
+
+    if (i >= stop) {
+        return;
+    }
+    //using data variable
+
+    //get item from the inifinte loop
+
+   
 
     
 
-    //get item from the inifinte loop
-    for (var i = 0; i < data.length; i++) {
+        //---LOOP BODY
+
+    var duration_css = "0.5";//seconds
+
+    var delay_interval = 2000;//milliseconds 
+
         //modifying html according to item
         var item = data[i];
-        debugger;
-        console.log("this is data");
-        console.log(data);
+        
+
         usr.picture = item.picture;
         usr.date = item.datetime;
         usr.msg = item.text;
         usr.name = item.fullname;
-        console.log("this is usr");
-        console.log(usr);
-       
-        var template = '<div class="mt-3 box seperator">' +
+
+    //using special css class for animation here
+        var template = '<div class="mt-3 box seperator newly animated fadeInDown delay-'+duration_css+'s  " id="newly' +
+            i +
+            '"' +
+            ">" +
             '   <div class="row">' +
             ' <div class="col-12" style="margin-top: 2%">' +
             ' <div class="user_profile_pic float-right mt-2">' +
@@ -74,39 +100,48 @@ function update()
             '  <div class="row">' +
             ' <div class="col-12">' +
             ' <div class="float-right poster_name float-right">' +
-            usr.name.toString() + '</div>' +
-
-
-            ' </div>' +
-            ' <div class="col-12">' +
-            '  <div class=" poster_date float-right mr-2 ">' + usr.date.toString() + '</div>' +
-
-            ' </div>' +
-            ' <div class="col-12">' +
-            ' <div class="float-right poster_text">' + usr.msg.toString() + '</div>' +
+            usr.name.toString() +
             '</div>' +
-
+            ' </div>' +
+            ' <div class="col-12">' +
+            '  <div class=" poster_date float-right mr-2 ">' +
+            usr.date.toString() +
+            '</div>' +
+            ' </div>' +
+            ' <div class="col-12">' +
+            ' <div class="float-right poster_text">' +
+            usr.msg.toString() +
+            '</div>' +
+            '</div>' +
             ' </div>' +
             '  </div>' +
-
             '  </div>' +
             ' </div>' +
             '  </div>';
-        console.log("template now is: ");
-        console.log(template);
-       
-
-        //inserting the code of the new item into HTML 
-        $("#append_here").prepend(template);
 
 
-        //{doing animation}
+        
 
 
-    }
+        
+
+    i++;//fowrding the index loop
+
+    //inserting the code of the new item into HTML 
+    //{doing animation}
+    delay(delay_interval).then(function () {
+         $("#append_here").after(template);
+
+        update();
+
+
+        });
+
+
+
+    
 
 }
-
 
 
 $(document).ready(init);
